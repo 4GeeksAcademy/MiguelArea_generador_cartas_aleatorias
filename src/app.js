d95fcddf55ebef9b19682335f696e523f92006d3
Miguel <miguelarea@gmail.com>
2025-05-24 19:51:24 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
-import "./style.css";
+function generarCarta() {
+  const palos = ['corazones', 'pica', 'trebol', 'diamante'];
+  const simbolos = { corazones: '♥', pica: '♠', trebol: '♣', diamante: '♦' };
+  const valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
+  const paloIndex = Math.floor(Math.random() * 4);
+  const valorIndex = Math.floor(Math.random() * 13);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const palo = palos[paloIndex];
+  const valor = valores[valorIndex];
+  const simbolo = simbolos[palo];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const cartaDiv = document.getElementById('carta');
+  cartaDiv.className = `card position-relative ${palo}`;
+  cartaDiv.innerHTML = `
+    <div class="corner-top-left">${simbolo}</div>
+    <div class="card-value">${valor}</div>
+    <div class="corner-bottom-right">${simbolo}</div>
+  `;
+}
